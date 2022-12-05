e4629b37999109d76554db6780e6e14312787e56
Peregrin <peregrinirus@mail.ru>
2022-12-06 00:53:44 +0300

    Разделяет код на модули

---
+++ src/components/popup.js
@@ -0,0 +1,18 @@
+export const createPopupTemplate = () => (
+  `<section class="film-details">
+  <form class="film-details__inner" action="" method="get">
+    <div class="form-details__top-container">
+      <div class="film-details__close">
+        <button class="film-details__close-btn" type="button">close</button>
+      </div>
+
+
+
+    </div>
+
+    <div class="form-details__bottom-container">
+
+    </div>
+  </form>
+</section>`
+);
